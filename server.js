d5345b9e0a4d641c2ef5879a8aebcc5d6742e870
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-02-13 11:00:09 -0500

    create route for nonAlcoholic.ejs

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/', (request, response) =>{
         response.render('index.ejs', {info: data})
     })
 })
+app.get('/nonAlcoholic', (request, response) =>{
+    db.collection('Specials').find().sort({price:1}).toArray()
+    .then(data => {
+        response.render('nonAlcoholic.ejs', {info: data})
+    })
+})
 app.get('/sangria', (request, response) =>{
     db.collection('Specials').find().sort({sequence:1}).toArray()
     .then(data => {
