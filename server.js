155c39af3c5d20c88dd9385abcff1514d5d112fc
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-02-16 13:38:36 -0500

    implement read functionality in beer.ejs

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/', (request, response) =>{
         response.render('index.ejs', {info: data})
     })
 })
+app.get('/beer', (request, response) =>{
+    db.collection('Specials').find().sort({price:1}).toArray()
+    .then(data => {
+        response.render('beer.ejs', {info: data})
+    })
+})
 app.get('/nonAlcoholic', (request, response) =>{
     db.collection('Specials').find().sort({price:1}).toArray()
     .then(data => {
