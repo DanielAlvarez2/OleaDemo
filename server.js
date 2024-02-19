051b197d11c1cdc5f898f4592c1f17bad2c3de8d
Daniel Yllanes <daniel.yllanes@hotmail.com>
2024-02-19 12:44:12 -0500

    create CSS columns in wineFormatLayout.ejs

--- server.js
+++ server.js
@@ -44,6 +44,12 @@ app.get('/', (request, response) =>{
         response.render('index.ejs', {info: data})
     })
 })
+app.get('/wineFormatLayout', (request, response) =>{
+    db.collection('Specials').find().sort({price:1}).toArray()
+    .then(data => {
+        response.render('wineFormatLayout.ejs', {info: data})
+    })
+})
 app.get('/beer', (request, response) =>{
     db.collection('Specials').find().sort({price:1}).toArray()
     .then(data => {
